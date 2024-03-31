30cff821ed999078609f14e4842fce5cbd17c77c
Gosudarev Danila <danidagosudarev@gmail.com>
2024-03-31 15:04:40 +0300

    path: '/*'

--- src/main.tsx
+++ src/main.tsx
@@ -39,7 +39,7 @@ const router = createBrowserRouter([
         element: <AboutMePage />,
       },
       {
-        path: "*",
+        path: "/*",
         element: <NotFound />,
       },
     ],
