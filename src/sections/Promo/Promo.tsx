4e02b2f3ee2af2f95e5a0c3b6b6e8a167b17eb32
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-22 12:41:12 +0300

    animed text of promo section

--- src/sections/Promo/Promo.tsx
+++ src/sections/Promo/Promo.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import cn from "classnames";
+import { motion } from "framer-motion";
 
 import { promoData } from "../../assets/data/data";
 import Button from "../../components/Button/Button";
@@ -7,27 +8,58 @@ import Socials from "../../components/Socials/Socials";
 
 import styles from "./Promo.module.scss";
 
+const bottomToTopAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.3,
+    },
+  }),
+};
+
 const Promo: FC = () => {
   return (
-    <section className={styles["promo"]}>
+    <motion.section
+      className={styles["promo"]}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
       <div className={cn("container", styles["promo__inner"])}>
         <div className={styles["promo__left-wrap"]}>
-          <h3 className={styles["promo__sub-title"]}>{promoData.subTitle}</h3>
-          <h1 className={styles["promo__title"]}>{promoData.title}</h1>
-          <div
-            className={cn(styles["promo__text"], styles["promo__text--1"])}
+          <motion.h3
+            className={styles["promo__sub-title"]}
+            variants={bottomToTopAnimation}
+            custom={1}
           >
+            {promoData.subTitle}
+          </motion.h3>
+          <motion.h1
+            variants={bottomToTopAnimation}
+            custom={2}
+            className={styles["promo__title"]}
+          >
+            {promoData.title}
+          </motion.h1>
+          <div className={cn(styles["promo__text"], styles["promo__text--1"])}>
             {promoData.text1}
           </div>
-          <div
-            className={cn(styles["promo__text"], styles["promo__text--2"])}
-          >
+          <div className={cn(styles["promo__text"], styles["promo__text--2"])}>
             {promoData.text2}
           </div>
-          <div className={styles["promo__row"]}>
+          <motion.div
+            variants={bottomToTopAnimation}
+            custom={3}
+            className={styles["promo__row"]}
+          >
             <Button path={promoData.btn.path}>{promoData.btn.text}</Button>
             <Socials />
-          </div>
+          </motion.div>
         </div>
         <div className={styles["promo__right-wrap"]}>
           <img
@@ -69,7 +101,7 @@ const Promo: FC = () => {
       </div>
       <div className={styles["promo__gradient-1"]} />
       <div className={styles["promo__gradient-2"]} />
-    </section>
+    </motion.section>
   );
 };
 
