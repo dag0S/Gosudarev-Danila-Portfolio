0f7bf201e114d4d6a0f02e350beae48932b10041
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-22 14:27:13 +0300

    animated tech stack section

--- src/components/TechnologyCard/TechnologyCard.tsx
+++ src/components/TechnologyCard/TechnologyCard.tsx
@@ -1,15 +1,21 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
+import { motion } from "framer-motion";
+
 import { TechnologyCardProps } from "./TechnologyCardProps";
 
 import styles from "./TechnologyCard.module.scss";
 
-const TechnologyCard: FC<TechnologyCardProps> = ({ item }) => {
+const TechnologyCard: FC<TechnologyCardProps> = forwardRef<
+  HTMLDivElement,
+  TechnologyCardProps
+>(({ item }, ref) => {
   return (
-    <div className={styles["card"]}>
+    <div className={styles["card"]} ref={ref}>
       <img src={item.icon} alt={item.name} />
       <div>{item.name}</div>
     </div>
   );
-};
+});
 
+export const MTechnologyCard = motion(TechnologyCard);
 export default TechnologyCard;
--- src/sections/TechStack/TechStack.tsx
+++ src/sections/TechStack/TechStack.tsx
@@ -1,35 +1,97 @@
 import { FC } from "react";
+import { motion } from "framer-motion";
+
+import { MTitle } from "../../components/Title/Title";
+import { MTechnologyCard } from "../../components/TechnologyCard/TechnologyCard";
+import { techStackData } from "../../assets/data/data";
 
 import styles from "./TechStack.module.scss";
-import Title from "../../components/Title/Title";
-import { techStackData } from "../../assets/data/data";
-import TechnologyCard from "../../components/TechnologyCard/TechnologyCard";
+
+const bottomToTopAnimation = {
+  hidden: {
+    y: 100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: custom * 0.3,
+    },
+  }),
+};
+
+const leftToRightAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible:{
+    x: 0,
+    opacity: 1,
+  },
+};
+
+const opacityAnimation = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: (custom: number) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: custom * 0.1,
+    },
+  }),
+};
 
 const TechStack: FC = () => {
   return (
-    <section className={styles["tech-stack"]}>
+    <motion.section
+      className={styles["tech-stack"]}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.3 }}
+    >
       <div className="container">
         <div className={styles["tech-stack__inner"]}>
-          <Title className={styles["tech-stack__title"]}>
+          <MTitle
+            className={styles["tech-stack__title"]}
+            variants={bottomToTopAnimation}
+          >
             {techStackData.title}
-          </Title>
+          </MTitle>
           <div className={styles["tech-stack__rows"]}>
             {techStackData.rows.map((row) => (
-              <div className={styles["tech-stack__row"]} key={row.id}>
-                <div className={styles["tech-stack__row-title"]}>
+              <motion.div
+                className={styles["tech-stack__row"]}
+                key={row.id}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.3 }}
+              >
+                <motion.div
+                variants={leftToRightAnimation}
+                className={styles["tech-stack__row-title"]}>
                   {row.title}
-                </div>
+                </motion.div>
                 <div className={styles["tech-stack__grid-icons"]}>
-                  {row.icons.map((item) => (
-                    <TechnologyCard key={item.id} item={item} />
+                  {row.icons.map((item, i) => (
+                    <MTechnologyCard
+                      key={item.id}
+                      item={item}
+                      variants={opacityAnimation}
+                      custom={i + 1}
+                    />
                   ))}
                 </div>
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
