f72b2f30e60498ff31f0e7e8f02c6e1a995c615d
Gosudarev Danila <danidagosudarev@gmail.com>
2024-04-27 17:24:44 +0300

    fix loading in aboit me page

--- src/pages/AboutMePage/AboutMePage.tsx
+++ src/pages/AboutMePage/AboutMePage.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { motion } from "framer-motion";
+import cn from "classnames";
 
 import Title from "../../components/Title/Title";
 import { aboutMePageData } from "../../assets/data/data";
+import { MProgressiveImage } from "../../components/ProgressiveImage/ProgressiveImage";
 
 import styles from "./AboutMePage.module.scss";
-import { MProgressiveImage } from "../../components/ProgressiveImage/ProgressiveImage";
 
 const leftToRightAnimation = {
   hidden: {
@@ -46,26 +47,22 @@ const AboutMePage: FC = () => {
         whileInView="visible"
         viewport={{ once: true, amount: 0.3 }}
       >
-        <div className="container">
-          <div className={styles["about-me__inner"]}>
-            <motion.div
-              className={styles["about-me__text-wrap"]}
-              variants={leftToRightAnimation}
-            >
-              <Title className={styles["about-me__title"]}>
-                {aboutMePageData.aboutMe.title}
-              </Title>
-              <div className={styles["text"]}>
-                {aboutMePageData.aboutMe.text}
-              </div>
-            </motion.div>
-            <MProgressiveImage
-              variants={rightToLeftAnimation}
-              placeholderSrc={aboutMePageData.aboutMe.img.tiny}
-              src={aboutMePageData.aboutMe.img.path}
-              alt={aboutMePageData.aboutMe.img.alt}
-            />
-          </div>
+        <div className={cn("container", styles["about-me__inner"])}>
+          <motion.div
+            className={styles["about-me__text-wrap"]}
+            variants={leftToRightAnimation}
+          >
+            <Title className={styles["about-me__title"]}>
+              {aboutMePageData.aboutMe.title}
+            </Title>
+            <div className={styles["text"]}>{aboutMePageData.aboutMe.text}</div>
+          </motion.div>
+          <MProgressiveImage
+            variants={rightToLeftAnimation}
+            placeholderSrc={aboutMePageData.aboutMe.img.tiny}
+            src={aboutMePageData.aboutMe.img.path}
+            alt={aboutMePageData.aboutMe.img.alt}
+          />
         </div>
         <div className={styles["about-me__gradient"]} />
       </motion.section>
@@ -73,46 +70,42 @@ const AboutMePage: FC = () => {
         className={styles["studies"]}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true, amount: 0.4 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
-        <div className="container">
-          <div className={styles["studies__inner"]}>
-            <motion.div
-              className={styles["text"]}
-              variants={rightToLeftAnimation}
-            >
-              {aboutMePageData.studies.text}
-            </motion.div>
-            <MProgressiveImage
-              variants={leftToRightAnimation}
-              placeholderSrc={aboutMePageData.studies.img.tiny}
-              src={aboutMePageData.studies.img.path}
-              alt={aboutMePageData.studies.img.alt}
-            />
-          </div>
+        <div className={cn("container", styles["studies__inner"])}>
+          <motion.div
+            className={styles["text"]}
+            variants={rightToLeftAnimation}
+          >
+            {aboutMePageData.studies.text}
+          </motion.div>
+          <MProgressiveImage
+            variants={leftToRightAnimation}
+            placeholderSrc={aboutMePageData.studies.img.tiny}
+            src={aboutMePageData.studies.img.path}
+            alt={aboutMePageData.studies.img.alt}
+          />
         </div>
       </motion.section>
       <motion.section
         className={styles["frontend"]}
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true, amount: 0.4 }}
+        viewport={{ once: true, amount: 0.3 }}
       >
-        <div className="container">
-          <div className={styles["frontend__inner"]}>
-            <motion.div
-              className={styles["text"]}
-              variants={leftToRightAnimation}
-            >
-              {aboutMePageData.frontend.text}
-            </motion.div>
-            <MProgressiveImage
-              variants={rightToLeftAnimation}
-              placeholderSrc={aboutMePageData.frontend.img.tiny}
-              src={aboutMePageData.frontend.img.path}
-              alt={aboutMePageData.frontend.img.alt}
-            />
-          </div>
+        <div className={cn("container", styles["frontend__inner"])}>
+          <motion.div
+            className={styles["text"]}
+            variants={leftToRightAnimation}
+          >
+            {aboutMePageData.frontend.text}
+          </motion.div>
+          <MProgressiveImage
+            variants={rightToLeftAnimation}
+            placeholderSrc={aboutMePageData.frontend.img.tiny}
+            src={aboutMePageData.frontend.img.path}
+            alt={aboutMePageData.frontend.img.alt}
+          />
         </div>
         <div className={styles["frontend__gradient"]} />
       </motion.section>
